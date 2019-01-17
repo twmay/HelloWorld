5095307495214751c7e1dcff24aaea054d095410
Tmay98 <twmay98@hotmail.com>
2019-01-16 17:57:17 -0800

    initial commit

---
+++ Poem.py
@@ -0,0 +1,2 @@
+print("Roses are red\nViolets are blue\nSugar is sweet\nBut I have commitment issues\n"
+      "So I’d rather just be friends\nAt this point in our relationship.")
---
+++ Temperature.py
@@ -0,0 +1,4 @@
+temperature_degrees_celsius = 10
+temperature_degrees_fahrenheit = (temperature_degrees_celsius*9/5) +32
+print("temperature in fahrenheit: ", temperature_degrees_fahrenheit)
+print("temperature in celsius: ", temperature_degrees_celsius)
